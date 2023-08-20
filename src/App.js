ba3698fbf4c101ed86c36d324392ff6f486dbc02
Anuj Jain <jainanuj744@gmail.com>
2023-08-20 21:36:55 +0530

    added Dynamic as well as Simple Import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import DynamicImport from './Components/DynamicImport'
-import './App.css';
+import DynamicImport from "./Components/DynamicImport";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Products from "./Pages/Products";
+import Testimonials from "./Pages/Testimonials";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import SimpleImport from './Components/SimpleImport';
 
 function App() {
   return (
     <div className="App">
-    <DynamicImport/>
-  
+      {/* <DynamicImport/> */}
+      {/* <SimpleImport/> */}
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="./" element={<Home />}></Route>
+          <Route path="./about" element={<About />}></Route>
+          <Route path="./products" element={<Products />}></Route>
+          <Route path="./testimonials" element={<Testimonials />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/DynamicImport.js
+++ src/Components/DynamicImport.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
+// import { moviesData } from "../data.js"
 
 export default function DynamicImport() {
+  let [movies, setMovies] = useState([]);
 
-    let [movies,setMovies] = useState([]);
-
-    let handleClick = (e) =>{
-        e.preventDefault();
-    }
-  return <>
-    <button onClick={handleClick}>Show Movies</button>
-    {
-        
-    }
-  </>;
+  let handleClick = (e) => {
+    e.preventDefault();
+    import("../data.js").then((data) => {
+      // console.log(data.moviesData);
+      setMovies(data.moviesData);
+    });
+  };
+  return (
+    <>
+      <button onClick={handleClick}>Show DYnamic Movies</button>
+      {movies.map((data) => {
+        return (
+          <>
+            <div key={data.id}>{data.name}</div>
+          </>
+        );
+      })}
+    </>
+  );
 }
