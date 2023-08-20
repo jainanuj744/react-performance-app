fc20e44edf6e5a3098489484311b5dfab53cc788
Anuj Jain <jainanuj744@gmail.com>
2023-08-20 21:38:05 +0530

    Simple Import Uncommented

--- src/Components/SimpleImport.js
+++ src/Components/SimpleImport.js
@@ -1,23 +1,23 @@
-// import { useState } from "react";
-// import { moviesData } from "../data.js";
+import { useState } from "react";
+import { moviesData } from "../data.js";
 
-// export default function SimpleImport() {
-//   let [movies, setMovies] = useState([]);
+export default function SimpleImport() {
+  let [movies, setMovies] = useState([]);
 
-//   let handleClick = (e) => {
-//     e.preventDefault();
-//     setMovies(moviesData);
-//   };
-//   return (
-//     <>
-//       <button onClick={handleClick}>Show Simple Movies</button>
-//       {movies.map((data) => {
-//         return (
-//           <>
-//             <div key={data.id}>{data.name}</div>
-//           </>
-//         );
-//       })}
-//     </>
-//   );
-// }
+  let handleClick = (e) => {
+    e.preventDefault();
+    setMovies(moviesData);
+  };
+  return (
+    <>
+      <button onClick={handleClick}>Show Simple Movies</button>
+      {movies.map((data) => {
+        return (
+          <>
+            <div key={data.id}>{data.name}</div>
+          </>
+        );
+      })}
+    </>
+  );
+}
